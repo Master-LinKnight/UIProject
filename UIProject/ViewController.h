5ab91adb7f5663b91610f064ecea498a1623ee0e
Knight <1051147003@qq.com>
2018-08-09 21:51:25 +0800

    Initial Commit

---
+++ UIProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIProject
+//
+//  Created by 林晓峰 on 2018/8/9.
+//  Copyright © 2018年 林晓峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIProject
+//
+//  Created by 林晓峰 on 2018/8/9.
+//  Copyright © 2018年 林晓峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
