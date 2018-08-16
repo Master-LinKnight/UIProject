578cfba67a2bd54efd51b3a6c4bccdbf74f5ad81
LinKnight <Knight.Lin@mkcorp.com>
2018-08-16 09:44:03 +0800

    增加按钮点击事件
    增加按钮点击事件

--- UIProject/ViewController.m
+++ UIProject/ViewController.m
@@ -18,6 +18,7 @@
     [self createLabel];
     [self createRectButtons];
     [self createUIView];
+    [self createButtonEvent];
 }
 
 - (void)createLabel {
@@ -27,30 +28,63 @@
     label.backgroundColor = [UIColor clearColor];
     label.font = [UIFont systemFontOfSize: 16];
     label.textColor = [UIColor blackColor];
-//    label.shadowColor = [UIColor grayColor];
-//    label.shadowOffset = CGSizeMake( 2, 2);
+    label.shadowColor = [UIColor grayColor];
+    label.shadowOffset = CGSizeMake( 1, 1);
     label.textAlignment = NSTextAlignmentLeft;
     label.numberOfLines = 0; //0会按照需要显示的行数进行显示
     [self.view addSubview:label];
 }
 
 -(void)createRectButtons {
-    UIButton* btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(100, 140, 100, 40);
     [btn setTitle:@"btn1" forState:UIControlStateNormal];
     [btn setTitle:@"btn2" forState:UIControlStateHighlighted];
+    [btn setImage:[UIImage imageNamed:@"arrow-1.png"] forState:UIControlStateNormal];
+    [btn setImage:[UIImage imageNamed:@"arrow-2.png"] forState:UIControlStateHighlighted];
     btn.backgroundColor = [UIColor clearColor];
     [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor orangeColor] forState:UIControlStateHighlighted];
+    [btn addTarget:self action:@selector(pressBtn:) forControlEvents:UIControlEventTouchUpInside];
     btn.titleLabel.font = [UIFont systemFontOfSize:12];
+    btn.tag = 100;
     [self.view addSubview:btn];
+}
+
+-(void)createButtonEvent {
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(100, 170, 100, 40);
+    [btn setTitle:@"btn03" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(pressBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [btn addTarget:self action:@selector(touchDown) forControlEvents:UIControlEventTouchDown];
+    btn.tag = 200;
+    [self.view addSubview:btn];
+}
+
+-(void) pressBtn: (UIButton*) btn {
+    if (btn.tag == 100) {
+        NSLog(@"btn01");
+    } else {
+        NSLog(@"btn03");
+    }
+    NSLog(@"%@", btn);
+}
+
+-(void) touchDown {
+    NSLog(@"按钮按下！");
 }
 
 -(void)createUIView {
     UIView* view = [[UIView alloc] init];
     view.frame = CGRectMake(40, 200, 300, 300);
     view.backgroundColor = [UIColor blueColor];
-    view.alpha = 0.5;
+//    view.alpha = 0.5;
+    UILabel* label = [[UILabel alloc] init];
+    label.text = @"Hello World!";
+    label.frame = CGRectMake(80, 100, 100, 40);
+    label.textColor = [UIColor blackColor];
+    label.backgroundColor = [UIColor redColor];
+    [view addSubview:label];
     [self.view addSubview:view];
 }
 
